a18d0ea82c9e2495b750faa93dbbee9d31f82ee6
Njuru22 <102201344+Njuru22@users.noreply.github.com>
2022-03-29 17:35:32 +0300

    Add files via upload

---
+++ five number.c
@@ -0,0 +1,12 @@
+//program to find area of a cirle 
+# include<stdio.h>
+int main()
+{
+	float radius;
+	float area;
+	printf("enter radius of circle\n");
+	scanf("%f",&radius);
+	area=3.14*radius*radius;
+	printf("area of a circle%f",area); 
+	
+}
