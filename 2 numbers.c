792c882f0209f2ea8e42a17c95a533f4bda2377c
Njuru22 <102201344+Njuru22@users.noreply.github.com>
2022-03-29 17:34:33 +0300

    Add files via upload

---
+++ 2 numbers.c
@@ -0,0 +1,17 @@
+//program to find the sum of the five numbers
+# include<stdio.h>
+int main()
+
+{
+	int y=10;
+	int x=4;
+	int w=5;
+	int n=7;
+	 int z=3;
+	int sum;
+	sum=z+y+x+w+n;
+	printf("sum of five numbers%i",sum);
+	
+	
+	return 0;
+}
