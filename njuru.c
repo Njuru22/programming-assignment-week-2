7c33dfcf50333af49c21dfd3626e7aaadf88c268
Njuru22 <102201344+Njuru22@users.noreply.github.com>
2022-03-29 17:33:48 +0300

    Add files via upload

---
+++ njuru.c
@@ -0,0 +1,18 @@
+//program to find the product of two numbers from the user
+# include<stdio.h>
+int main()
+
+{
+	int d;
+	int e;
+	int product;
+	printf("enter first number\n");
+	scanf("%d",&d);
+	printf("enter second number\n");
+	scanf("%d",&e);
+	product=d*e;
+	printf("product of d and e is %d",product);
+	
+	
+	return 0;
+}
