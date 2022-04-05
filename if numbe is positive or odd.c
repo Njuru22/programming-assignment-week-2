db9eaf06684e143bc0dbcf0007178968d428e7b1
Njuru22 <102201344+Njuru22@users.noreply.github.com>
2022-04-05 19:28:00 +0300

    Add files via upload

---
+++ if numbe is positive or odd.c
@@ -0,0 +1,20 @@
+//if a number is even or odd
+#include <stdio.h>
+int main()
+{
+	int number;
+	printf("enter a number");
+	scanf("%d",& number);
+	if(number >0)
+	{
+		printf("the number is positive");
+	}
+	else 
+	{
+		printf("the number is negative");
+	}
+
+			
+	
+	return 0;
+}
