149de89de4c3383594c98081a25bbccef5d31343
Njuru22 <102201344+Njuru22@users.noreply.github.com>
2022-04-19 12:08:51 +0300

    Create Loop.c

---
+++ Loop.c
@@ -0,0 +1,13 @@
+//for loop
+//a program to output the first 50 natural numbers in reverse
+#include <stdio.h>
+void main(){
+  int r;
+  printf("the first 50 numbers in reverse for a loop\n");
+for(r=1;r<=50;r--)///for initialize;condition;decrement)
+{
+  print"("%d\n");
+  }
+  printf("\n");
+
+  return 0;
