c77fed0b09bb3789d2e7870c9a57cff18d660216
Njuru22 <102201344+Njuru22@users.noreply.github.com>
2022-04-21 18:03:01 +0300

    Add files via upload

---
+++ if numbe is even or odd.c
@@ -0,0 +1,20 @@
+//discount on cost
+#include <stdio.h>
+int main()
+{
+	float cost,discount;
+	printf("enter cost");
+	scanf("%f",& cost);
+	if(cost >1000)
+	{
+	discount=0.1*cost	
+	}
+	else 
+	{
+	discount=0*cost
+	}
+
+			
+	
+	return 0;
+}
